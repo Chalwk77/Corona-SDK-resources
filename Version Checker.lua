f8b1a972a8fa854a073bcbfcfd4c285a94bd7dbb
Jericho <Chalwk77@users.noreply.github.com>
2018-03-06 09:56:38 +1300

    Update Version Checker.lua

--- Version Checker.lua
+++ Version Checker.lua
@@ -1,5 +1,6 @@
 -- requires that the google play webstore listing description contains "Current App Version: digit(decimal)digit(decimal)digit+"
 
+local current_build = system.getInfo("appVersionString")
 local function networkListener(event)
     if (event.isError) then
         print('Network error:', event.response)
@@ -8,24 +9,24 @@ local function networkListener(event)
         if (string.find(data, "Current App Version: %d%.%d.%d+")) then
             local replace = "Current App Version: %d%.%d.%d+"
             local google_play_version = string.gsub(string.match(data, replace), replace, string.match(string.match(data, replace), "%d%.%d.%d+"))
-            local version = {current = current_build, updated = google_play_version}
+            local version = {current = current_build, latest_update = google_play_version}
             local height_from_bottom = -100
-            if version ~= nil and version.updated ~= nil then
-                if (string.find(version.updated, "%d%.%d.%d+") == 1) then
-                    if (version.current == version.updated) then
+            if version ~= nil and version.latest_update ~= nil then
+                if (string.find(version.latest_update, "%d%.%d.%d+") == 1) then
+                    if (version.current == version.latest_update) then
                         application_version = display.newText( "Version " .. version.current, display.viewableContentWidth / 2, display.viewableContentHeight / 2, native.systemFontBold, 10 )
                         application_version:setFillColor(1, 0.9, 0.5)
                         application_version.x = display.contentCenterX
                         application_version.y = display.contentCenterX + display.contentCenterY - height_from_bottom
                         application_version.alpha = 0.50
-                    elseif (version.current < version.updated) then
+                    elseif (version.current < version.latest_update) then
                         local function onTextClick( event )
                             if ( event.phase == "began" ) then
                                 showDialog("Download Latest Update", "Would you like to download the latest update?", 18, true)
                             end
                             return true
                         end
-                        application_version = display.newText( "Version " .. version.updated .. " is available!", display.viewableContentWidth / 2, display.viewableContentHeight / 2, native.systemFontBold, 10 )
+                        application_version = display.newText( "Version " .. version.latest_update .. " is available!", display.viewableContentWidth / 2, display.viewableContentHeight / 2, native.systemFontBold, 10 )
                         application_version:setFillColor(1, 0.9, 0.5)
                         application_version.x = display.contentCenterX
                         application_version.y = display.contentCenterX + display.contentCenterY - height_from_bottom
@@ -43,6 +44,6 @@ local function networkListener(event)
 end
 
 function CheckForUpdates()
-    local data = network.request("link to google play webstore listing", "POST", networkListener)
+    local data = network.request("link to google play webstore app listing", "POST", networkListener)
 end
 CheckForUpdates()
